e3753688ff00e50a778e8ce6ae4bc974a536cf60
waccess.krivtsov <O;O$'LMMRt7^GDjH1M,=>
2022-05-18 14:20:31 +0300

    KBASE: added springBoot starter files

---
+++ src/main/java/com/home/knowbase/KnowBaseApplication.java
@@ -0,0 +1,13 @@
+package com.home.knowbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowBaseApplication.class, args);
+	}
+
+}
