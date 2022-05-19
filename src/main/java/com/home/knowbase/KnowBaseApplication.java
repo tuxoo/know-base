c8b706a0beae879a361851d071fe6ee6f3038ea2
eugene <kia-77@mail.ru>
2022-05-19 10:05:30 +0300

    KBASE: added build properties endpoint to spring actuator

--- src/main/java/com/home/knowbase/KnowBaseApplication.java
+++ src/main/java/com/home/knowbase/KnowBaseApplication.java
@@ -1,11 +1,14 @@
 package com.home.knowbase;
 
+import com.home.knowbase.properties.ApplicationProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperty.class)
 public class KnowBaseApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KnowBaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KnowBaseApplication.class, args);
+    }
 }
--- src/main/java/com/home/knowbase/controller/UtilController.java
+++ src/main/java/com/home/knowbase/controller/UtilController.java
@@ -1,15 +1,20 @@
 package com.home.knowbase.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/util")
+@RequiredArgsConstructor
 public class UtilController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "ping";
+    private final BuildProperties buildProperties;
+
+    @GetMapping("/version")
+    public String version() {
+        return buildProperties.getVersion();
     }
 }
